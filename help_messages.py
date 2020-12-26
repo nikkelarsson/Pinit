5cf473ab1ed5223ac65803d5b103df034ab78400
Niklas Mikael <nikke.larsson@gmail.com>
2020-12-26 17:40:59 +0200

    removed dir non-interactive and at the same time moved every file a dir back

---
+++ help_messages.py
@@ -0,0 +1,7 @@
+# Optional arguments
+help_msg =    '''-h, --help \t\t\t\t\t show this message and exit\n'''
+version_msg = '''-V, --version \t\t\t\t print program version and exit\n'''
+readme_msg =  '''    --add-readme \t\t\t\t create README.md file\n'''
+gitign_msg =  '''    --add-gitignore \t\t\t create .gitignore file\n'''
+# Mandatory arguments
+project_msg = '''NAME \t\t\t\t\t\t\t define name for the project\n'''
