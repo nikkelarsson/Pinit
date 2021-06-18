bcb105085122beb5c7aff2e569524f04f69a5eea
Niklas Mikael <nikke.larsson@gmail.com>
2021-06-18 10:38:24 +0300

    Did cleanup and countless other things.
    - Added couple notes about compatibilty and other things.
    
    - Removed unnecessary comments.
    
    - Renamed variables better.
    
    - Made a lot of things more concise.

--- src/cmds/create.py
+++ src/cmds/create.py
@@ -1,94 +1,129 @@
 
-'''
-File : create.py  
-Desc : File creation procedures.
-'''
+"""
+ *  File: create.py  
+ *  Desc: Project / creation procedures.
+ *  Auth: Niklas Larsson
+"""
+
+# TODO: Windows compatibility? - This is mostly about the backslashes,
+# as Unix based systems uses forward slashes.
+
+# TODO: Is it actually necessary to be able to create projects to custom
+# locations? Because probably the use case is usually that, when one
+# wishes to create a new project user already is in the right location thus,
+# it kind of would be just useless, no?
 
 import getpass
 import os
 import string
 
+ENCODING: str = "utf-8"
+USERNAME: str = getpass.getuser()
 
-# NOTE: pass arguments for ”*files” as a list or as a tuple!
-# This is, because files() tries to parse the *files
-# argument as a list (or tuple): giving *files just a single variable
-# as an argument would make create() work unexpectedly.
-def files(*files: tuple, language: str='eng', verbose: bool=False,
-		project_type: str='python', git: bool=False) -> str:
-
-	'''Create the files that make the project, like the root dir etc.'''
-
-	count: int = 0
-	username: str = getpass.getuser()
-	fwd_slashes: int = 0
-
-	# TODO: 1. Make sure filename is valid. [CHECK]
-	# TODO: 2. If creating in other location than cwd make sure that the location actually exists.
-	# TODO: 3. Finally make sure, that no file with the same name exists in the location.
-	for new_file in files:
-		if count == 0:
-
-			# Allow using the ”~/” abbreviation: it's nicer to write less.
-			if new_file.startswith('~/'):
-				new_file.replace('~/', '/Users/{}/'.format(username))
-
-			# To avoid problems, prohibit the use of ”-” as a prefix in filenames.
-			if new_file.startswith('-'):
-				return 'INVALID_FILENAME'
-
-			# Check if file is created in location other than cwd.
-			# This is unfinished.
-			if fwd_slashes >= 1:
-				for final_fwd_slash in new_file:
-					if final_fwd_slash:
-						pass
-
-			if os.path.exists(new_file[0]):
-				return 'FILE_ALREADY_EXISTS'
+INVALID_PREFIXES: tuple = (
+		"@", "*", "-", "=", "+", "&", "€", "/",
+		"\\", "^", "(", ")", "[", "]", "{", "}"
+		)
+INVALID_SUFFIXES: tuple = (
+		"@", "*", "-", "=", "+", "&", "€", "/",
+		"\\", "^", "(", ")", "[", "]", "{", "}"
+		)
 
 
+def project(name: str, language: str="eng", git: bool=False) -> str:
+	"""Create new python project."""
+	forward_slashes: bool = "/" in name
 
+	if language == "fi":
+		success_msg: str = "Luotiin uusi projekti ”{}”.".format(name.lower())
+	elif language == "eng":
+		success_msg: str = "Created new project ”{}”.".format(name.lower())
 
+	# To avoid problems, prohibit use of
+	# certain characters as prefix and suffix.
+	if name.startswith(INVALID_PREFIXES):
+		return "PROJECT_NAME_INVALID"
+	elif name.endswith(INVALID_SUFFIXES):
+		return "PROJECT_NAME_INVALID"
 
+	# Allow using the ”~/” abbreviation; it's nicer to write less.
+	# This is used because os module does not allow using the ~/
+	# abbreviation, so it must be done manually here.
+	elif name.startswith("~/"):
+		name.replace("~/", "/Users/{}/".format(USERNAME))
 
-			# NOTE: WHAT THIS SECTION BELOW EVEN DOES?
-			# ----------------------------------------
-			# Check whether project was created to cwd or else.
-			# Reason: When we create a project in cwd, we get the
-			# project's name straight away. But when we create a
-			# project somewhere else, the actual name of the project
-			# is the last word in the path -> path/to/myproject ->
-			# ”myproject” and that is the word we need to extract
-			# from the path.
-			# for filename in new_file:
-			# 	letters = filename[0:].split()
-			# 	for letter in letters:
-			# 		if letter == '/':
-			# 			fwd_slashes += 1
-			# return letters
+	if forward_slashes:
+		# Split the path, for example /Users/<user>/Documents/, like:
+		# "", "Users", "<user>", "Documents", ""
+		path_split: list = name.split("/")
+		# Iterate through the given path and, if any section of the
+		# given path is invalid, return an error; there is no need
+		# to check the validity of every part of the given path as,
+		# if any of the parts turns out to be invalid, then we already
+		# know, that the path is not going to be valid anyway.
+		for part in path_split:
+			# When the path user provides starts, or ends with a forward slash,
+			# those first and last forward slashes are going to be empty
+			# items in the path_split, thus, they need to be ignored, as they
+			# would count as invalid sections in the path.
+			#
+			# First, check the first section of the path, like: /Users/
+			# Then, check the second, like: /Users/<user>/, and so on...
+			if part != "":
+				if not os.path.exists(part):
+					return "INVALID_PATH"
 
+		# After validating the path, check that no existing
+		# project with same name exists in the location.
+		if os.path.exists(name):
+			return "PROJECT_EXISTS"
+		# Project-dir
+		os.mkdir(name.lower())
+		# Source-dir
+		os.mkdir("{}/src".format(name.lower()))
+		# Readme
+		with open("{}/README.md".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("# {}".format(name.title()))
+		# Changelog
+		with open("{}/CHANGELOG.md".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("# Changelog")
+		# src/main.py
+		with open("{}/src/main.py".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("\n\"\"\"\n")
+			f.write("File: main.py  \nDesc: --  \nAuth: {} <email>".format(USERNAME))
+			f.write("\n\"\"\"\n")
+			f.write("\n\ndef main() -> None:\n\tpass\n")
+			f.write("\n\nif __name__ == \"__main__\":\n\tmain()")
+		# src/__init__.py
+		with open("{}/src/__init__.py".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("\n\"\"\"\n")
+			f.write("File: __init__.py  \nDesc: --")
+			f.write("\n\"\"\"")
+		return success_msg
 
-			# Its a good convention to name directories and files using lowercase letters only.
-			# project = file[0].lower()
-
-			# try:
-			#     os.mkdir(project) # This will be the root directory.
-			# except FileNotFoundError:
-			#     return 'FILE_NOT_FOUND'
-
-			# with open('{}/README.md'.format(project), 'w', encoding='utf-8') as f:
-			#     f.write('# {}'.format(project))
-			# with open('{}/CHANGELOG.md'.format(project), 'w', encoding='utf-8') as f:
-			#     f.write('# Changelog')
-			# os.mkdir('{}/src'.format(project)) # And this will be the src dir.
-
-			# return 'OK.'
-			#count += 1
-
-		# if count == 1:
-		#     if language == 'fi':
-		#         msg = 'Luotiin ”{}” onnistuneesti.'.format(project)
-		#     elif language == 'eng':
-		#         msg = 'Created ”{}” successfully.'.format(project)
-		#     if verbose == True:
-		#         return msg
+	else:
+		if os.path.exists(name):
+			return "PROJECT_EXISTS"
+		# Project-dir
+		os.mkdir(name.lower())
+		# Source-dir
+		os.mkdir("{}/src".format(name.lower()))
+		# Readme
+		with open("{}/README.md".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("# {}".format(name.title()))
+		# Changelog
+		with open("{}/CHANGELOG.md".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("# Changelog")
+		# src/main.py
+		with open("{}/src/main.py".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("\n\"\"\"\n")
+			f.write("File: main.py  \nDesc: --  \nAuth: {} <email>".format(USERNAME))
+			f.write("\n\"\"\"\n")
+			f.write("\n\ndef main() -> None:\n\tpass\n")
+			f.write("\n\nif __name__ == \"__main__\":\n\tmain()")
+		# src/__init__.py
+		with open("{}/src/__init__.py".format(name.lower()), "w", encoding=ENCODING) as f:
+			f.write("\n\"\"\"\n")
+			f.write("File: __init__.py  \nDesc: --")
+			f.write("\n\"\"\"")
+		return success_msg
