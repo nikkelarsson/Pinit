bd5addf00b6d208da174f7b244fe56d0248d2aab
Niklas Mikael <nikke.larsson@gmail.com>
2021-06-18 10:58:42 +0300

    Few small updates.

--- src/errs/file.py
+++ src/errs/file.py
@@ -1,14 +1,15 @@
 
 '''
-File : file.py
-Desc : File operation related errors.
+File: file.py
+Desc: File operation related errors.
 '''
+
+import sys
 
 
 def exists(filename: str, language: str='eng') -> None:
 	'''Give an error when trying to create a file
 	with a name that matches already existing file's name.'''
-
 	if language == 'fi':
 		msg = 'Virhe: ”{}” on jo olemassa'.format(filename)
 	elif language == 'eng':
@@ -16,20 +17,19 @@ def exists(filename: str, language: str='eng') -> None:
 	print(msg)
 
 
-def invalid_filename(filename: str, language: str='eng') -> None:
+def invalid_name(filename: str, language: str='eng') -> None:
 	'''Give an error when attempting to create file
 	with invalid name.'''
-
 	if language == 'fi':
 		msg = 'Virhe: Huono nimeämiskäytäntö ”{}”'.format(filename)
 	elif language == 'eng':
 		msg = 'Error: Bad naming convention ”{}”'.format(filename)
 	print(msg)
+	sys.exit(1)
 
 
 def name_missing(language: str='eng') -> None:
 	'''Give an error when file is missing a filename.'''
-
 	if language == 'fi':
 		msg = 'Virhe: <nimi> puuttuu'
 	elif language == 'eng':
@@ -40,7 +40,6 @@ def name_missing(language: str='eng') -> None:
 def not_found(path: str, language: str='eng') -> None:
 	'''Give an error when trying to create files to location
 	that doesn't exist.'''
-
 	if language == 'fi':
 		msg = 'Virhe: Virheellinen polku ”{}”'.format(path)
 	elif language == 'eng':
