0023db1c771251722f536d652fe3dc76cc7933a8
Niklas Mikael <nikke.larsson@gmail.com>
2021-06-18 10:36:02 +0300

    Updated file header etc.
    - Added a small table of contents at the beginning of the file.
    
    - Updated some little syntactic things.

--- src/cmds/__init__.py
+++ src/cmds/__init__.py
@@ -1,7 +1,22 @@
-'''
-File : __init__.py  
-Desc : Catalog of available commands and flags.
-'''
+
+"""
+ *  File: __init__.py  
+ *  Desc: Catalog of available commands and flags.
+ *  Auth: Niklas Larsson
+"""
+
+#
+# Description information.
+#
+# < Commands >
+# 1. create..........Create new project.
+#
+#
+# < Flags >
+# 1. help............Print command's and flag's descriptions.
+# 2. version.........Print program's version.
+# 3. silent..........Don't print any output, except errors.
+# 4. summary.........Print a summary what was created.
 
 # Imports here are sorted alphabetically.
 import cmds.create
@@ -9,14 +24,15 @@ import cmds.help
 import cmds.version
 
 # Available commands and flags. Maybe sort them here alphabetically too?
-commands : dict = {}
-flags    : dict = {}
+commands: dict = {}
+flags: dict = {}
 
 # Commands
-commands['create'] = {'long' : 'create'}
+commands["create"] = {"long" : "create"}
 
 # Flags
-#flags['git'] = {'long' : '--init-as-git'}
-flags['help']      = {'short' : '-h', 'long' : '--help'}
-flags['version']   = {'short' : '-V', 'long' : '--version'}
-flags['verbose']   = {'short' : '-v', 'long' : '--verbose'}
+flags["git"] = {"long" : "--init-as-git"}
+flags["help"] = {"short" : "-h", "long" : "--help"}
+flags["version"] = {"short" : "-V", "long" : "--version"}
+flags["silent"] = {"short" : "-s", "long" : "--silent"}
+flags["summary"] = {"short" : "-S", "long" : "--summary"}
